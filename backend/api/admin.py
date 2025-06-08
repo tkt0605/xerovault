a748daf4d6f60e2663110560c751ef9885c6518d
tkt0605 <takato.komada2005@outlook.jp>
2025-06-08 22:59:21 +0900

    apiのインデクスページのアクセス完了

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,30 +2,23 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin as baseUserAdmin
 class CustomUserAdmin(baseUserAdmin):
-    list_display = ['email', 'is_staff', 'is_active', 'is_superuser']
+    list_display = ['email', 'avater', 'is_staff', 'is_active', 'is_superuser']
     list_filter = ['is_staff', 'is_active']
     fieldsets = (
         (None, {
-            'fields': ('email', 'password', 'avatar', 'is_staff', 'is_active', 'is_superuser')
+            'fields': ('email', 'avater', 'password', 'is_staff', 'is_active', 'is_superuser')
             }
         ),
         ('Permissions', {
             'fields': ('groups', 'user_permissions')
             }
         ),
-        ('Important Dates', {
-            'fields': ('last_login', 'date_joined')
-            }
-        ),
-        ('Advanced Oprions', {
-            'fields': ('id', ),
-            }
-        )
     )
     add_fieldsets = (
-        (None,{'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}),
+        (None,{'fields': ('email','avater' , 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}),
     )
-    search_fields = ['email', 'id']
+    search_fields = ['email',]
     ordering = ['email',]
+    readonly_fields = ['id']
 admin.site.register(CustomUser, CustomUserAdmin)
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.parsers import MultiPartParser, FormParser
 from .models import GeneratePublicToken, CustomUser, GenerateGroup
 from rest_framework.views import APIView
-from .serializers import RegisterSerialiser, CustomUserSerializer, CustomUserDetairsSerializer, EmailLoginSerializer, LoginSerializer, LogoutSerializer, GeneratePublicTokenSerializer, GenerateGroupSerializer, GenerateGroupReadSerializer, GeneratePublicTokenReadSerializer
+from rest_framework.generics import CreateAPIView
+from .serializers import RegisterSerializer, CustomUserSerializer, CustomUserDetairsSerializer, EmailLoginSerializer, LoginSerializer, LogoutSerializer, GeneratePublicTokenSerializer, GenerateGroupSerializer, GenerateGroupReadSerializer, GeneratePublicTokenReadSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from datetime import timedelta
 from django.contrib.auth import get_user_model
@@ -14,26 +15,26 @@ User = get_user_model()
 class EmailLoginAPI(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
-        serialiser = EmailLoginSerializer(data=request.data)
-        if serialiser.is_valid():
-            user = serialiser.validated_data
+        serializer = EmailLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
             refresh = RefreshToken.for_user(user)
             return Response({
                 "access": str(refresh.access_token),
                 "refresh": str(refresh),
             })
-class RegisterAPI(APIView):
+class RegisterAPI(CreateAPIView):
     permission_classes = [AllowAny]
-    serialiser_class = RegisterSerialiser
+    serializer_class = RegisterSerializer
     queryset = CustomUser.objects.all()
 class LogoutAPI(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
-        serialiser = LogoutSerializer(data=request.data)
-        if serialiser.is_valid():
-            serialiser.save()
+        serializer = LogoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response({'message': 'ログアウトしました。'}, status=200)
-        return Response(serialiser.errors, status=400)
+        return Response(serializer.errors, status=400)
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
@@ -47,19 +48,19 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return CustomUserDetairsSerializer
         return super().get_serializer_class()
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get'], permission_classes = [IsAuthenticated])
     def me(self, request):
         user = request.user
         serializer = CustomUserDetairsSerializer(user)
         return Response(serializer.data)
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes = [IsAuthenticated])
     def upload_avatar(self, request):
         user = request.user
-        serialiser = CustomUserDetairsSerializer(user, data=request.data, partial=True)
-        if serialiser.is_valid():
-            serialiser.save()
-            return Response(serialiser.data, status=200)
-        return Response(serialiser.errors, status=400)
+        serializer = CustomUserDetairsSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
 class GeneratePublicTokenViewSet(viewsets.ModelViewSet):
     queryset = GeneratePublicToken.objects.all()
     serializer_class = GeneratePublicTokenSerializer
@@ -72,7 +73,7 @@ class GeneratePublicTokenViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return GeneratePublicTokenReadSerializer
         return super().get_serializer_class()
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes = [IsAuthenticated])
     def generate_token(self, request):
         serializer = GeneratePublicTokenSerializer(data=request.data)
         if serializer.is_valid():
@@ -93,17 +94,16 @@ class GenerateGroupviewSet(viewsets.ModelViewSet):
         return super().get_serializer_class()
     def perform_create(self, serializer):
         serializer.save(founder=self.request.user)
-    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes = [IsAuthenticated])
     def create_group(self, request):
         serializer = GenerateGroupSerializer(data=request.data)
         if serializer.is_valid():
             group = serializer.save(founder=self.request.user)
             return Response('グループが作成されました。', status=201)
         return Response(serializer.errors, status=400)
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get'], permission_classes = [IsAuthenticated])
     def my_groups(self, request):
         user = self.request.user
         groups = GenerateGroup.objects.filter(founder=user)
         serializer = GenerateGroupReadSerializer(groups, many=True)
         return Response(serializer.data)
-
