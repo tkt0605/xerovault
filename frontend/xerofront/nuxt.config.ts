f0c95c920df646d2be9b05a4a16ec7db041187b8
tkt0605 <takato.komada2005@outlook.jp>
2025-06-09 21:37:17 +0900

    Nuxt.js3の設定変更

--- frontend/xerofront/nuxt.config.ts
+++ frontend/xerofront/nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
@@ -10,7 +9,7 @@ export default defineNuxtConfig({
         {charset: 'utf-8'},
         {name: 'viewport', content:'width=device-width, initial-scale=1'},
         {name: 'description', content: 'Xerovault は、Xero API 資格情報用の安全な保管庫です。'},
-        {name: 'keywards', content: 'Xero、API、資格情報、保管庫、セキュリティ'},
+        {name: 'keywords', content: 'Xero、API、資格情報、保管庫、セキュリティ'},
       ],
       link: [
         {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
@@ -27,15 +26,19 @@ export default defineNuxtConfig({
     "@/assets/css/main.css",
   ],
   modules: [
-    // '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next',
     '@pinia/nuxt',
   ],
   runtimeConfig: {
     public: {
       apiBase: 'http://localhost:8000/api/'
+    }
+  },
+  i18n: {
+    experimental: {
+      bundle: {
+        optimizeTranslationDirective: false // または true（使いたい場合）
+      }
     }
   }
 })
