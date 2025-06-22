4154d53bd15df97387c5af4f629fb323d9a38a6f
tkt0605 <takato.komada2005@outlook.jp>
2025-06-22 21:57:56 +0900

    QR表示でのダイヤログの表示

--- frontend/xerofront/components/Aside.vue
+++ frontend/xerofront/components/Aside.vue
@@ -146,7 +146,7 @@
           </div>
         </template>
         <template #footer>
-          <button @click="createGroup"
+          <button @click="createNewGroup"
             class="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-md">作成する</button>
         </template>
       </Dialog>
